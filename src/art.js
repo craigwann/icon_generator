20642a494944f91a9b47dd5906cf43f5c185ebef
Craig Wann and Eric Shull <eric@>
2018-08-08 16:42:36 -0700

    add functionality to API call. Retrieving objects from call

---
+++ src/art.js
@@ -0,0 +1,47 @@
+// 
+// export class ArtService{
+//   constructor(query) {
+//     this.query = query
+//   }
+//
+//
+//   function search(this.query) {
+//         return $.getJSON("https://www.rijksmuseum.nl/api/nl/collection?q=Q&key=fpGQTuED&format=json".replace("Q", this.query));
+//       }
+//
+//       // creates a thumbnail image for the specified art object
+//       function thumbnail(object) {
+//         return $("<div>")
+//           .addClass("thumb")
+//           .css("background-image", "url(" + object.webImage.url.replace("s0", "s128") +")");
+//       }
+//
+//       // fire the search query
+//       search($("#query").val())
+//         .done(function(results) {
+//           $("#example3-container").empty();
+//
+//           var $table = $("#example3-container");
+//           $table.html("");
+//
+//           // create a row for each art object found
+//           $.each(results.artObjects, function(index, object) {
+//             console.log(object);
+//
+//             var $row = $('<tr class="child"><td>'
+//               + object.objectNumber
+//               +'</td><td class="thumbnail">'
+//               +'</td><td>'
+//               + object.title
+//               +'</td></tr>').appendTo($table);
+//
+//             $row.find(".thumbnail").append(thumbnail(object));
+//
+//             // make each row clickable, navigating to the relevant page on the Rijksmuseum website
+//             $row.on("click", function() {
+//               document.location = object.links.web;
+//             });
+//           })
+//         });
+//
+// }
--- src/main.js
+++ src/main.js
@@ -3,28 +3,30 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-import { Icon } from './icon_api';
+// import { ArtService } from './art';
 
 $(document).ready(function() {
 
   // let input = $("#iconName").find('input[name=icon]').val();
-  let input = "cat"
 
 
     let request = new XMLHttpRequest();
-    let url = `https://www.rijksmuseum.nl/api/en/collection?key=${process.env.API_KEY}&format=json&type=schilderij&f.normalized32Colors.hex=%20%23367614`
+    // let url = `https://www.rijksmuseum.nl/api/en/collection?key=${process.env.API_KEY}&format=json&type=schilderij&f.normalized32Colors.hex=%20%23367614`
     // let url = `http://api.giphy.com/v1/gifs/search?api_key=qwCP7BLZ5HMzmejAQLmGB0REUMKUK5b1&q=${petName}`;
+    let url = `https://www.rijksmuseum.nl/api/nl/collection?q=rembrandt&key=${process.env.API_KEY}&format=json`;
     let getElements = function(response) {
-      let randomNumber = Math.floor(Math.random() * 3);
+
+      let randomNumber = Math.floor(Math.random() * 25);
       var icon = response.artObjects[randomNumber];
       // document.getElementById('creature_gif').innerHTML = "<img src=" + icon + ">";
     }
 
     request.onreadystatechange = function() {
+
       if (this.readyState === 4 && this.status === 200) {
         let response = JSON.parse(this.responseText);
-
-        console.log(response);
+        console.log(JSON.parse(this.response));
+        // console.log(response);
         getElements(response);
       }
     }
