af68441f0553e7d2903a0eaadbd1865734f6450e
Craig Wann and Eric Shull <craig@>
2018-08-08 11:57:28 -0700

    add form to index.html

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+import { Icon } from './icon_api';
+
+$(document).ready(function() {
+
+  let input = $("#iconName").find('input[name=icon]').val();
+  
+
+});
