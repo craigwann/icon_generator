e1da32733b83b5d55216db295473842e353d027d
Craig Wann and Eric Shull <craig@>
2018-08-09 11:53:12 -0700

    add refactoring for API call to put the request in a separate class

--- src/art.js
+++ src/art.js
@@ -1,47 +1,18 @@
-// 
-// export class ArtService{
-//   constructor(query) {
-//     this.query = query
-//   }
-//
-//
-//   function search(this.query) {
-//         return $.getJSON("https://www.rijksmuseum.nl/api/nl/collection?q=Q&key=fpGQTuED&format=json".replace("Q", this.query));
-//       }
-//
-//       // creates a thumbnail image for the specified art object
-//       function thumbnail(object) {
-//         return $("<div>")
-//           .addClass("thumb")
-//           .css("background-image", "url(" + object.webImage.url.replace("s0", "s128") +")");
-//       }
-//
-//       // fire the search query
-//       search($("#query").val())
-//         .done(function(results) {
-//           $("#example3-container").empty();
-//
-//           var $table = $("#example3-container");
-//           $table.html("");
-//
-//           // create a row for each art object found
-//           $.each(results.artObjects, function(index, object) {
-//             console.log(object);
-//
-//             var $row = $('<tr class="child"><td>'
-//               + object.objectNumber
-//               +'</td><td class="thumbnail">'
-//               +'</td><td>'
-//               + object.title
-//               +'</td></tr>').appendTo($table);
-//
-//             $row.find(".thumbnail").append(thumbnail(object));
-//
-//             // make each row clickable, navigating to the relevant page on the Rijksmuseum website
-//             $row.on("click", function() {
-//               document.location = object.links.web;
-//             });
-//           })
-//         });
-//
-// }
+
+export class ArtService{
+  getArtByInput(input){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://www.rijksmuseum.nl/api/en/collection?q=${input}&key=${process.env.API_KEY}&format=json&imgonly=true`;
+      request.onload = function() {
+        if (this.status === 200) {
+         resolve(request.response);
+       } else {
+         reject(Error(request.statusText));
+       }
+      }
+      request.open("GET", url, true);
+      request.send();
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,61 +3,62 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-// import { ArtService } from './art';
+import { ArtService } from './art';
 
 $(document).ready(function() {
 
   $('#searchTerm').submit(function(event) {
     event.preventDefault();
+    console.log("form submit");
+    let input = $("#searchTerm").find('input[name=work]').val();
+    $('#searchTerm').val("");
+    let artService = new ArtService();
+    console.log("art service " + ArtService);
+    let promise = artService.getArtByInput(input);
+    console.log(promise);
+    // let request = new XMLHttpRequest();
+    // let url = `https://www.rijksmuseum.nl/api/en/collection?q=${input}&key=${process.env.API_KEY}&format=json&imgonly=true`;
+    // let getElements = function(response) {
+    //   let array = response.artObjects
+    //   let works = []
+    //   for (var i = 0; i < array.length; i++) {
+    //     $('#artwork').append("<img src=" + array[i].webImage.url + ">");
+    //     }
+    //   }
+      promise.then(function(response) {
+        console.log("response " + response );
+       let body = JSON.parse(response);
+       console.log("body" + body);
+       let array = body.artObjects;
+       console.log("craig");
+       let works = [];
+       for (var i = 0; i < array.length; i++) {
+         console.log("here");
+         $('#artwork').append("<img src=" + array[i].webImage.url + ">");
+         }
 
 
-
-  let input = $("#searchTerm").find('input[name=work]').val();
-console.log(input);
-
-    let request = new XMLHttpRequest();
-    let url = `https://www.rijksmuseum.nl/api/en/collection?q=${input}&key=${process.env.API_KEY}&format=json&imgonly=true`;
-    let getElements = function(response) {
-
-      // let randomNumber = Math.floor(Math.random() * 25);
-      let array = response.artObjects
-      let works = []
-
-      // array.forEach((piece) => {
-      //   $("#artwork").append(`<img src="${piece.webImage.url}">`);
-      // });
-
-      var art = response.artObjects[2].webImage.url;
-
-      for (var i = 0; i < array.length; i++) {
-        //console.log(array[i].webImage.url);
-        $('#artwork').append("<img src=" + array[i].webImage.url + ">");
-        // works.push(array[i].webImage.url)
-      }
-
-      // console.log("array " + works);
-      // document.getElementById('artwork').innerHTML = "<img src=" + works[1] + ">"
-      //
-      // // $(".artwork").innerHTML = "<li>" + art + "</li>";
-      // console.log("the " + art);
-      // document.getElementById('artwork').innerHTML = "<img src=" + art + ">"
-      // // document.getElementById('creature_gif').innerHTML = "<img src=" + icon + ">";
-
-
-    }
-
-    request.onreadystatechange = function() {
-
-      if (this.readyState === 4 && this.status === 200) {
-        let response = JSON.parse(this.responseText);
-        console.log(JSON.parse(this.response));
-        // console.log(response);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
+      });
 
   });
 });
+// function(error) {
+// $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+//  };
+
+
+
+// promise.then(function(response) {
+//   console.log("response " + response );
+//  let body = JSON.parse(response);
+//  console.log("body" + body);
+//  let getElements = function(response) {
+//    let array = response.artObjects;
+//    let works = [];
+//    for (var i = 0; i < array.length; i++) {
+//      console.log("here");
+//      $('#artwork').append("<img src=" + array[i].webImage.url + ">");
+//    }
+//  }
+//
+// });
